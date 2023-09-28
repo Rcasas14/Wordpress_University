65fcdadce05d86c4c8c8ef3967569dc41a7938aa
Reymart Casas <rcasas@axxess.com>
2023-09-29 07:57:58 +0800

    updated from 9.29 at 8am

--- themes/casas-de-university-theme/single-event.php
+++ themes/casas-de-university-theme/single-event.php
@@ -17,12 +17,24 @@ while(have_posts()){
 <div class="container container--narrow page-section">
     <div class="metabox metabox--position-up metabox--with-home-link">
         <p>
-            <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('event'); ?>"><i class="fa fa-home"
-                    aria-hidden="true"></i> Event Home</a> <span
+            <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('event'); ?>"><i
+                    class="fa fa-home" aria-hidden="true"></i> Event Home</a> <span
                 class="metabox__main"><?php the_title(); ?></span>
         </p>
     </div>
     <div class="generic-content"><?php the_content(); ?></div>
+    <?php 
+    $relatedPrograms = get_field('related_programs'); // this variable is array
+    echo'<h2 class="headline headline--medium"></h2>'
+    echo '<ul class="link-list min-list">';
+    foreach($relatedPrograms as $program){ //to get each index 
+        ?>
+    <li><a href="<?php echo get_permalink($program); ?>"><?php echo get_the_title($program) ?>;</a></li>
+    <?php }
+    echo '</ul>';
+    echo '</h2>';
+    ?>
+
 </div>
 
 <?php }
